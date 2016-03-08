1741c21b8a1cf9694897e6cd78f4ee32d57fe698
jeremyhilliard14 <jeremyhilliard14@gmail.com>
2016-03-07 21:12:23 -0500

    eliminated several divs
    added png  images down at bottom

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 var playerTurn = 1;
+
+
 function setMark(element) {
-	alert("clicked on " + element);
-}
+	if (playerTurn === 1){
+		if (document.getElementById('a1').style.display === "") {
+			document.getElementById('x').style.display === "block";
+		};
+	};
+};
+
+// var icons = document.getElementById('app-icons');
+//        if(icons.style.display == 'block')
+//           icons.style.display = 'none';
+//        else
+//           icons.style.display = 'block';
+//   }
+// 	alert("clicked on " + element);
+// }
